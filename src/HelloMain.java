1638103cedf1cd04b4ebda3a80e474f2523b1dcd
Jisang you <jisangman89@gmail.com>
2017-09-04 14:55:09 +0900

    first commit

---
+++ src/HelloMain.java
@@ -0,0 +1,20 @@
+
+public class HelloMain {
+
+	// 접근제한자 리턴타입 함수명 (String) {
+	// public
+	// private 자기 클래스안에서만
+	// protected
+	// (default)
+	// }
+
+	
+	public static void main(String args[]){
+	
+		System.out.println("HelloJava!!!");
+		
+	}
+	
+	
+	
+}
